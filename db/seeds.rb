96216199574a071ad3934d61f0ea9c25e6901e7c
estiben <estibenmontoyataborda@gmail.com>
2022-09-19 18:39:24 -0500

    exercici JV sabado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+2.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
