b032d9d33cf8738f8c2dca39ee3b536cd17b8177
estiben <estibenmontoyataborda@gmail.com>
2022-09-22 19:49:49 -0500

    bustrap

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select"
 
+
 export default class extends Controller {
   connect() {
     new TomSelect(this.element, {})
