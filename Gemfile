2c0c617e1e4bc557eacbea5364a7408eae1116ea
estiben <estibenmontoyataborda@gmail.com>
2022-09-22 21:09:42 -0500

    lits

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 gem "dotenv-rails", groups: [:development, :test]
+gem "cloudinary"
 # gem 'foreman', '~> 0.87.2'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,17 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   has_many :reviews, dependent: :destroy
-
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
+
+  validate :upload_is_image
+
+  private
+
+  def upload_is_image
+    return if photo && photo.content_type =~ %r{/^image/(jpeg|pjpeg|png|bmp)$/}
+
+    photo.purge
+    errors.add(:photo, ': This is not a valid photo')
+  end
 end
